16f69e4192bc07a42caeacd7274536d606590839
Arvid Paulsson <ar8115pa-s@student.lu.se>
2023-03-03 15:44:42 +0100

    Added some basic pages

---
+++ src/Contact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div className="Contact">
+          <div></div>
+          <h1>Contact information to be added</h1>
+          <div className="card">
+            <p>Hello!</p>
+          </div>
+        </div>
+      );
+}
+
+export default Contact; 
