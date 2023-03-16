c98d565fb98fc0dc7511ab11841fc192b7a3eb93
Arvid Paulsson <ar8115pa-s@student.lu.se>
2023-03-16 16:46:30 +0100

    test branch

--- src/Login.tsx
+++ src/Login.tsx
@@ -42,7 +42,7 @@ const Login = () => {
         </Button>
         <FormControl display="flex" alignItems="center">
           <FormLabel htmlFor="dark_mode" mb="0">
-            Dark?
+            DarkMode
           </FormLabel>
           <Switch
             id="dark_mode"
