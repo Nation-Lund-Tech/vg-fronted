7006ef9586f0e64e9179331c050ca29456561541
danieljohannsson <daniel.johannsson1999@gmail.com>
2023-05-09 15:50:05 +0200

    error message and VGs logo added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Link as ReachLink, useLocation, useNavigate } from "react-router-dom";
+import { Link as ReachLink, json, useLocation, useNavigate } from "react-router-dom";
 import {
   Flex as Center,
   Heading,
@@ -11,10 +11,13 @@ import {
   useColorModeValue,
   Link,
   getToken,
+  FormHelperText,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAuth } from "../providers/AuthProvider";
 import Layout from "../components/Layout";
+import { set } from "react-hook-form";
 
 const Login = () => {
   const { user, signin } = useAuth();
@@ -24,6 +27,7 @@ const Login = () => {
   const formBackground = useColorModeValue('gray.100', 'gray.700');
   var [email, setEmail] = useState<string>("");
   var [passwd, setPasswd] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   const from = location.state?.from || "/";
 
@@ -34,18 +38,19 @@ const Login = () => {
   }, [user]);
 
   const onLogin = async () => {
+    setError("");
     const response = await fetch("https://localhost:7008/api/Auth/login", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: email, password: passwd })
     })
 
-    const token = await response.text();
-
-    if (token) {
+    if (response.status == 200) {
+      const { token } = await response.json();
       signin(token);
+    } else if (response.status == 400) {
+      setError("Wrong email or password. Try again!");
     }
-
   }
 
   /*const getUsername = async () => {
@@ -71,24 +76,37 @@ const Login = () => {
           boxShadow="lg"
         >
           <Heading mb={6}>Log In</Heading>
-          <Input
-            placeholder={"example@mail.com"}
-            type="email"
-            variant="filled"
-            mb={3}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <Input
-            placeholder="**********"
-            type="password"
-            variant="filled"
-            mb={6}
-            onChange={(event) => setPasswd(event.target.value)}
-          />
-          <Button onClick={onLogin} colorScheme="teal" mb={8}>
+          <FormControl isInvalid={error !== ""} mb="1rem">
+            <FormLabel></FormLabel>
+            <Input
+              placeholder={"example@mail.com"}
+              type="email"
+              variant="filled"
+              mb={3}
+              onChange={(event) => {
+                setEmail(event.target.value)
+                setError("");
+              }}
+              />
+            <FormLabel></FormLabel>
+            <Input
+              placeholder="**********"
+              type="password"
+              variant="filled"
+              onChange={(event) => {
+                setPasswd(event.target.value)
+                setError("");
+              }}
+            />
+            {error !== "" && (
+              <FormErrorMessage>Wrong email or password. Try again!</FormErrorMessage>
+            )}
+          </FormControl>
+
+          <Button onClick={onLogin} colorScheme="yellow" mb={8}>
             Log In
           </Button>
-          <FormControl display="flex" alignItems="center">
+          {/* <FormControl display="flex" alignItems="center">
             <FormLabel htmlFor="dark_mode" mb="0">
               DarkMode
             </FormLabel>
@@ -98,7 +116,7 @@ const Login = () => {
               size="lg"
               onChange={toggleColorMode}
             />
-          </FormControl>
+          </FormControl> */}
         </Center>
       </Center>
     </Layout>
