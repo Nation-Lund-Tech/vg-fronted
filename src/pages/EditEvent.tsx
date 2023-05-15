d758c0f54d962ff7e2ab279814c66a08c4ea9a6d
William <william.reit@gmail.com>
2023-05-15 16:32:32 +0200

    minor progress

--- src/pages/EditEvent.tsx
+++ src/pages/EditEvent.tsx
@@ -1,12 +1,55 @@
 import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
+import { ThankEvent, WorkEvent } from "../Common/Types";
+
+
+const { eventId } = useParams<{ eventId: string }>();
+
+const fetchWorkEvents = async (): Promise<WorkEvent> => {
+    let workEvent: WorkEvent;
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/WorkEvent/id/${eventId}`);
+        workEvent = await response.json();
+        return workEvent;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {} as WorkEvent;
+    }
+}
+
+const fetchThankEvents = async (): Promise<ThankEvent> => {
+    let thankEvent: ThankEvent;
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/ThankEvent/id/${eventId}`);
+        thankEvent = await response.json();
+        return thankEvent;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {} as ThankEvent;
+    }
+}
 
 export default function EditEvent() {
-    const { eventId } = useParams<{ eventId: string }>();
+    const [event, setEvent] = useState({} as Event); // [state, setState
+
+    // get the event from the id
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BASE_URL}/api/Event/${eventId}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setEvent(data);
+            });
+    }, [eventId]);
+
 
     return (
         <Layout>
-            <h1>Edit event {eventId}</h1>
+            {/* show all the info of the event and make it editable */}
+            
         </Layout>
     );
 }
