51242b154c9db7c56f3cf7e17e06ffb75967b932
danieljohannsson <daniel.johannsson1999@gmail.com>
2023-05-17 15:55:46 +0200

    scrollbar added

--- src/pages/WorkList.tsx
+++ src/pages/WorkList.tsx
@@ -19,6 +19,8 @@ import { useEffect, useState } from "react";
 import React from "react";
 import Layout from "../components/Layout";
 import { useAuth } from "../providers/AuthProvider";
+import container from "*.css"
+import "../styles.css"
 
 function WorkList() {
 
@@ -39,104 +41,112 @@ function WorkList() {
     const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/Worker/delete/${email}`, {
       method: "DELETE",
     });
-    if(response.status === 200) {
+    if (response.status === 200) {
       setWorkers(workers?.filter((worker) => worker.email !== email));
     }
   }
 
   return (
     <Layout>
-    <Center>
-    <VStack
-      divider={<StackDivider />}
-      p="4"
-      w="100%"
-      maxW={{ base: "100vw", sm: "80vw", lg: "70vw", xl: "60vw" }}
-      alignItems="stretch"
-    >
-      <HStack justifyContent="space-between">
-        <Heading as="h1" size="2xl" textAlign="center" my={4}>
-          Workers
-        </Heading>
-        <Link to={"/add-worker"}>
-          <Button colorScheme="green" leftIcon={<AddIcon />}>
-            Create new
-          </Button>
-        </Link>
-      </HStack>
-      <Input 
-        placeholder="Search by name"
-        size="md"
-        onChange={(event) => {
-          setSearch(event.target.value.toLowerCase())
-        }}
-        />
-      {/* {worker &&
-      <HStack>
-          <Link to={"/arbetare/${employee.id}"}>
-            <Text as="a">{worker.firstName}</Text>
-          </Link>
-          <Text>{worker.email}</Text>
-          <Spacer />
-          <Button colorScheme="red">Remove</Button>
-        </HStack>
-} */}
-      {workers &&
-        workers.filter((worker) => 
-        (`${worker.email} ${worker.firstName} ${worker.lastName}`)
-        .toLowerCase()
-        .includes(search))
-        .sort((w1, w2) => w2.lastUpdate.localeCompare(w1.lastUpdate))
-        .map((worker) => (
-          <HStack key={worker.id}>
-          <VStack alignItems="flex-start">
-            <Link to={`/arbetare/${worker.id}`}>
-            <HStack>
-              <Icon as={MdPerson} w={5} h={5} />
-              <Text as="a">{`${worker.firstName} ${worker.lastName}`}</Text>  
-            </HStack>
+      <Center>
+        <VStack
+
+          divider={<StackDivider />}
+          p="4"
+          w="100%"
+          maxW={{ base: "100vw", sm: "80vw", lg: "70vw", xl: "60vw" }}
+          alignItems="stretch"
+        >
+          <HStack justifyContent="space-between">
+            <Heading as="h1" size="2xl" textAlign="center" my={4}>
+              Workers
+            </Heading>
+            <Link to={"/add-worker"}>
+              <Button colorScheme="green" leftIcon={<AddIcon />}>
+                Create new
+              </Button>
             </Link>
+          </HStack>
+          <Input
+            placeholder="Search by name or email"
+            size="md"
+            onChange={(event) => {
+              setSearch(event.target.value.toLowerCase())
+            }}
+          />
+          
+            <VStack
+              divider={<StackDivider />}
+              maxH={["60vh", "48vh"]}
+              p="4"
+              style={{
+                overflowX: "hidden",
+                overflowY: "scroll",
+              }}
+              w="100%"
+              maxW={{ base: "100vw", sm: "80vw", lg: "70vw", xl: "60vw" }}
+              alignItems="stretch"
+            >
+        
+              {workers &&
+                workers.filter((worker) =>
+                  (`${worker.email} ${worker.firstName} ${worker.lastName}`)
+                    .toLowerCase()
+                    .includes(search))
+                  .sort((w1, w2) => w2.lastUpdate.localeCompare(w1.lastUpdate))
+                  .map((worker) => (
+                    <HStack key={worker.id}>
+                      <VStack alignItems="flex-start">
+                        <Link to={`/arbetare/${worker.id}`}>
+                          <HStack>
+                            <Icon as={MdPerson} w={5} h={5} />
+                            <Text as="a">{`${worker.firstName} ${worker.lastName}`}</Text>
+                          </HStack>
+                        </Link>
+                        <HStack>
+                          <Icon as={MdEmail} w={5} h={5} />
+                          <Text as="a">{worker.email}</Text>
+                        </HStack>
+                        <HStack>
+                          <Icon as={MdAttachMoney} w={5} h={5} />
+                          <Text as="a">{worker.bank}</Text>
+                        </HStack>
+                        <HStack>
+                          <Icon as={MdUpdate} w={5} h={5} />
+                          <Text as="a">{new Date(worker.lastUpdate).toLocaleDateString()}</Text>
+                        </HStack>
+                      </VStack>
+                      <Spacer />
+                      {auth.user?.role == "Admin" && (
+                        <>
+                          <Button colorScheme="red" size={"sm"} onClick={() => removeWorker(worker.email)}>Remove</Button>
+                        </>
+                      )}
+                    </HStack>
+                  ))}
+            </VStack>
+            
+        
             <HStack>
-            <Icon as={MdEmail} w={5} h={5}/> 
-            <Text as="a">{worker.email}</Text>
+
+              <Link to={"/register-work"}>
+                <Button size="md">
+                  Register work
+                </Button>
+              </Link>
+
+              <Spacer />
+
+              <Link to={"/register-tack"}>
+                <Button size="md">
+                  Register reward
+                </Button>
+              </Link>
+
             </HStack>
-            <HStack>
-            <Icon as={MdAttachMoney} w={5} h={5} />
-              <Text as="a">{worker.bank}</Text>
-          </HStack>
-          <HStack>
-            <Icon as={MdUpdate} w={5} h={5} />
-          <Text as="a">{new Date(worker.lastUpdate).toLocaleDateString()}</Text>
-          </HStack>
-          </VStack>
-          <Spacer />
-          {auth.user?.role == "Admin" && (
-          <>
-            <Button colorScheme="red" size={"sm"} onClick={() => removeWorker(worker.email)}>Remove</Button>  
-          </>
-        )}
-          </HStack>
-        ))}
-      <HStack>
-
-        <Link to={"/register-work"}>
-          <Button size="md">
-            Register work
-          </Button>
-        </Link>
-
-        <Spacer />
-
-        <Link to={"/register-tack"}>
-        <Button size="md">
-          Register reward
-        </Button>
-        </Link>
-
-      </HStack>
-    </VStack>
-    </Center>
-    </Layout>
+        </VStack>
+      </Center>
+    </Layout >
   );
 }
 
