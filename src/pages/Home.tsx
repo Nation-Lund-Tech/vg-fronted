ee5817015c34ce1a6f74b26c779cf838438646c4
danieljohannsson <daniel.johannsson1999@gmail.com>
2023-08-04 11:45:00 +0200

    Changed Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import Layout from "../components/Layout"
+import { Heading } from "@chakra-ui/react";
 
 const Home = () => {
-    return <Layout><h1>Home</h1></Layout>;
-  };
-  
+  return <Layout>
+    <Heading as="h1" size="2xl" textAlign="center" my={4}>
+      Welcome to Västgöta Nation!
+    </Heading>
+  </Layout>;
+};
+
 export default Home;
